d06ccdbb11770b08676ea3418da0c996098a1d3f
sly <stravio123@gmail.com>
2026-01-18 00:05:42 +0000

    Add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	platform: 'gitea',
+	endpoint: 'https://example.com/api/v1/', // set this to the url of your gitea instance
+	gitAuthor: 'Renovate Bot <renovate-bot@example.com>', // set the email address to whatever email your gave this user in your gitea
+	username: 'renovate-bot',
+	autodiscover: true,
+	onboardingConfig: {
+		$schema: 'https://docs.renovatebot.com/renovate-schema.json',
+		extends: ['config:recommended'],
+	},
+	optimizeForDisabled: true,
+	persistRepoData: true,
+};
